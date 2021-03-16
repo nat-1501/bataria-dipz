468cc23ff9df6947128dfc1ce21a96dd0d69b1ad
Natali Soares <natalisoares90@gmail.com>
2021-03-16 20:15:50 -0300

    adicionando conexão do banco mysql

--- Conexao.php
+++ Conexao.php
@@ -1,1 +1,11 @@
-    
+<?php
+
+try  {
+    //faz conexao com o banco de dados
+
+      $conectar = new PDO ("mysql:host=localhost;port=3306;dbname=pdo;", "root", "");
+
+}  catch (PDOException $e) {
+
+    echo 'Falha:' . $e->getMessage();
+} 
